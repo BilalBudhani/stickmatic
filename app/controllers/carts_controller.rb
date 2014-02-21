970797c968c27df06103c342f87846b7f4f4d76b
Pritesh Jain <prit.jain8@gmail.com>
2014-02-21 10:03:43 +0530

    added update quantity in cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,36 @@
 class CartsController < ApplicationController
   def index
   end
-  def remove
-    if @pack = Pack.find_by_id(params[:item_id])
-      @cart.remove(@pack)
+
+  def update
+    if params[:cart_item].present? && params[:cart_item][:quantity].present? && params[:cart_item][:item_id].present?
+      quantity = params[:cart_item][:quantity].to_i
+      update_cart(@item,quantity) if  quantity > 0 and @item = Pack.find_by_id(params[:cart_item][:item_id])
+    end
+
+    respond_to do |format| 
+      format.html {redirect_to basket_path}
+      format.js
     end
   end
+
+
+  def remove
+    if @item = Pack.find_by_id(params[:item_id])
+      @cart.remove(@item)
+    end
+    respond_to do |format| 
+      format.html {redirect_to basket_path}
+      format.js
+    end
+
+  end
+
+  private 
+
+  def update_cart(item,quantity)
+        @cart.remove(@item)
+        @cart.add(item, item.price,quantity)
+  end
+
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,8 @@
 class Cart < ActiveRecord::Base
   acts_as_shopping_cart_using :cart_item
+
+  def tax_pct
+    0
+  end
+
 end
