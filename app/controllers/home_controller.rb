ba50389a6334e9c6f65ed98a5cce72243e06b47c
BilalBudhani <bilalbudhani@gmail.com>
2014-02-22 07:44:55 +0530

    Select method removed and some refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,6 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, only: [:select]
 
   def index
-    redirect_to select_path if current_user
-  end
-
-  def select
-    instagram_client
-    @pack = Pack.new
-    (9 - @pack.pack_items.length).times {@pack.pack_items.build}
+    redirect_to new_pack_path if current_user
   end
 end
--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -1,26 +1,38 @@
 class PacksController < ApplicationController
+
+  before_filter :instagram_client, only: [:new, :edit]
+
+  def new
+    @pack = Pack.new
+    (pack_length - @pack.pack_items.length).times {@pack.pack_items.build}
+  end
+
   def create
     @pack = Pack.new(pack_params)
     @pack.user_id = current_user.id
     if @pack.save!
       @cart.add(@pack, @pack.price)
       flash[:success] = "Pack added to cart"
-      redirect_to select_path
+      redirect_to new_pack_path
     else
-      redirect_to select_path
+      redirect_to new_pack_path
     end
   end
 
   def edit
-    instagram_client
     @pack = Pack.find(params[:id])
-    (9 - @pack.pack_items.length).times {@pack.pack_items.build}
+    (pack_length - @pack.pack_items.length).times { @pack.pack_items.build }
   end
 
   def update
   end
 
+  private
   def pack_params
-    params.require(:pack).permit(:pack_items_attributes=>[:instagram_id,:instagram_image_url])
+    params.require(:pack).permit(pack_items_attributes: [:instagram_id,:instagram_image_url])
+  end
+
+  def pack_length
+    9
   end
 end
