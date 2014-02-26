4b713fae6fb75faeda4eda462a7ede1f36aa1014
Pritesh Jain <prit.jain8@gmail.com>
2014-02-26 14:39:04 +0530

    change should to expect syntax

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,20 +2,21 @@ require 'spec_helper'
 
 describe User do
   it "has a valid factory" do
-    FactoryGirl.create(:user).should be_valid
+    expect(FactoryGirl.create(:user)).to be_valid
   end
 
   it "is invalid without a provider" do
-        FactoryGirl.build(:user, provider: nil).should_not be_valid
+        expect(FactoryGirl.build(:user, provider: nil)).not_to be_valid
   end
 
   it "is invalid without a username" do
-    FactoryGirl.build(:user, username: nil).should_not be_valid
+    expect(FactoryGirl.build(:user, username: nil)).not_to be_valid
   end
 
   it "is invalid without a auth token" do
-    FactoryGirl.build(:user, token: nil).should_not be_valid
+    expect(FactoryGirl.build(:user, token: nil)).not_to be_valid
   end
 
   it "has many carts"
+
 end
