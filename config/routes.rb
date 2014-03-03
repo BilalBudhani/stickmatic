63911e3e50e007ff450224f93cee3f5d2f1eb21c
BilalBudhani <bilalbudhani@gmail.com>
2014-03-03 19:35:38 +0530

    Fixes order index rendering

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Stickmatic::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :packs
-
+  resource :order, only: [:update]
   root 'home#index'
   get '/basket' , to: "orders#index", as: 'basket'
 end
