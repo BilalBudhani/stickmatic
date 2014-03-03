2d3068cc0efd67dc26a97d26193a39551f4edc90
BilalBudhani <bilalbudhani@gmail.com>
2014-03-03 19:18:57 +0530

    Removes unsed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,30 +2,4 @@ class OrdersController < ApplicationController
   def index
     @order = Order.find_by(user: current_user, paid: false)
   end
-
-  def update
-    if params[:cart_item].present? && params[:cart_item][:quantity].present? && params[:cart_item][:item_id].present?
-      quantity = params[:cart_item][:quantity].to_i
-      update_cart(@item,quantity) if  quantity > 0 and @item = Product.find_by_id(params[:cart_item][:item_id])
-    end
-
-    respond_to do |format|
-      format.html {redirect_to basket_path}
-      format.js
-    end
-  end
-
-  def remove
-    if @item = Product.find_by_id(params[:item_id])
-      @cart.remove(@item)
-    end
-    redirect_to basket_path
-  end
-
-  private
-
-  def update_cart(item,quantity)
-    @cart.remove(@item)
-    @cart.add(item, item.price,quantity)
-  end
 end
